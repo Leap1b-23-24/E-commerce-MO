624aa007d1d8c08985c409405bbe26a0ace27b81
Irmuun Temuulel <23LP3813@G301-CC0199.local>
2024-03-11 10:06:19 +0800

    Header

--- front/src/Components/Header/Header.tsx
+++ front/src/Components/Header/Header.tsx
@@ -3,16 +3,20 @@ import Image from "next/image";
 export const Header = () => {
   return (
     <div className="bg-teal-500 w-full">
-      <div className="flex w-full flex justify-center items-center">
+      <div className="flex w-full justify-center items-center gap-12">
         <div className="flex gap-3 p-4">
           <Image alt="" src="/pinelogo.png" width={28} height={28} />
-          <h1 className="text-white">Ecommerce</h1>
+          <h1 className="text-white text-2xl">Ecommerce</h1>
         </div>
         <div className="">
-          <Image alt="" src="/search.png" width={24} height={24} />
-          <input className="rounded" type="text" placeholder="Хайлт" />
+          {/* <Image alt="" src="/search.png" width={24} height={24} /> */}
+          <input className="rounded-lg p-2" type="text" placeholder="Хайлт" />
         </div>
-        <div></div>
+        <div className="flex">
+          <Image alt="" src="/heart.png" width={50} height={50} />
+          <Image alt="" src="/shop.png" width={50} height={50} />
+          <Image alt="" src="/Profile.png" width={50} height={50} />
+        </div>
       </div>
     </div >
   );
