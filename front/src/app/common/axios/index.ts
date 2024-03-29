f4fc01b0b2e90cb52af8f3059d6f6c2a626c6825
Munkh-Orgil Lkhavgajav <23LP0045@G301-CC0203.local>
2024-03-29 10:59:54 +0800

    update db url

--- front/src/app/common/axios/index.ts
+++ front/src/app/common/axios/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/",
+  // baseURL: "http://localhost:3001/",
+  baseURL: "https://team2-omae.onrender.com/",
   headers: { "Content-Type": "application/json" },
 });
