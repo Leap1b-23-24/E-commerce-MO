b47da706974d30d208b598dbca7c1b34d8ea5b22
Orgil <104488839+Morgild@users.noreply.github.com>
2024-05-16 09:01:41 +0800

    Update

--- front/src/Components/LoadingPage.tsx
+++ front/src/Components/LoadingPage.tsx
@@ -7,10 +7,8 @@ export const LoadingPage = () => {
       height={"100vh"}
       bgcolor={"common.white"}
       justifyContent={"center"}
-      alignItems={"start"}
+      alignItems={"center"}
     >
-      <Typography fontsize={24}>Please Wait</Typography>
-      <Typography fontsize={24}>...Loading Data</Typography>
       <CircularProgress />
     </Stack>
   );
